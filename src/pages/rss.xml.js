ef8222766be83dd0c09d21ab21df90ec9eead716
Sam <contact@codeyverse.com>
2022-10-07 20:08:04 +1100

    Removed site title and description from rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,9 @@
 import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
 export const get = () =>
 	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		title: "Codey Verse Blog",
+		description: "A website where I blog my experience building Codey Verse, an open source website where people can learn to code.",
 		site: import.meta.env.SITE,
 		items: import.meta.glob('./blog/**/*.{md,mdx}'),
 	});
