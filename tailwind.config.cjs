ba4bebcbe7ea0e27687ec9204e47d77c1653e5a2
Sam <contact@codeyverse.com>
2022-10-07 19:11:38 +1100

    Added colour theme and navbar
    Also removed footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary": "#0E2349",
+				"secondary": "#16336A",
+				"accent": "#BBD2FF",
+				"neutral": "#191D24",
+				"base-100": "#12203A",
+				"brand": "#4B7DDB",
+				"success": "#09D769",
+				"error": "#F52C2C",
+			}
+		}
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography')],
 }
